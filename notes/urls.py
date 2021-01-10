dbe13a55acbf721e90d126bf5d428a5bfad3d740
AbnocChinwads <rnemo@hotmail.co.uk>
2021-01-10 15:00:46 +0000

    tried to condense views into one view

--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.notes, name='notes'),
-    path('add-notes/', views.add_notes, name='add-notes'),
-    path('open-notes/', views.open_notes, name='open-notes'),
+    path('', views.view_or_open_notes, name='notes'),
+    path('add-notes/', views.view_or_open_notes, name='add-notes'),
+    path('open-notes/', views.view_or_open_notes, name='open-notes'),
+    path('edit-note/<int:docid>/', views.view_or_open_notes, name='edit-note'),
+    path('delete-note/<int:docid>/', views.delete_note, name='delete-note'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,13 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect, reverse
 
 from .models import Document
 
 
-def notes(request):
+def view_or_open_notes(request):
     """
     A view to show all campaign notes for the user,
     including sorting and search queries
     """
+
+    docid = int(request.GET.get('docid', 0))
+    documents = Document.objects.all()
+
+    if request.method == 'POST':
+        docid = int(request.POST.get('docid', 0))
+        title = request.POST.get('title')
+        content = request.POST.get('content', '')
+
+        if docid > 0:
+            document = Document.objects.get(pk=docid)
+            document.title = title
+            document.content = content
+            document.save()
+
+            return redirect('notes')
+        else:
+            document = Document.objects.create(title=title, content=content)
+
+            return HttpResponseRedirect(reverse('notes'))
+
+    if docid > 0:
+        document = Document.objects.get(pk=docid)
+    else:
+        document = ''
+
+    context = {
+        'docid': docid,
+        'documents': documents,
+        'document': document,
+    }
+
+    if docid != 0:
+        return render(request, 'open-notes.html', context)
+    else:
+        return render(request, 'notes.html', context)
+
+
+"""def add_notes(request):
+
+    A view to show all campaign notes for the user,
+    including sorting and search queries
+
 
     docid = int(request.GET.get('docid', 0))
     documents = Document.objects.all()
@@ -17,38 +60,11 @@ def notes(request):
         'documents': documents,
     }
 
-    return render(request, 'notes.html', context)
+    return render(request, 'add-notes.html', context)"""
 
 
-def add_notes(request):
-    """
-    A view to show all campaign notes for the user,
-    including sorting and search queries
-    """
+def delete_note(request, docid):
+    document = Document.objects.get(pk=docid)
+    document.delete()
 
-    docid = int(request.GET.get('docid', 0))
-    documents = Document.objects.all()
-
-    context = {
-        'docid': docid,
-        'documents': documents,
-    }
-
-    return render(request, 'add-notes.html', context)
-
-
-def open_notes(request):
-    """
-    A view to show all campaign notes for the user,
-    including sorting and search queries
-    """
-
-    docid = int(request.GET.get('docid'))
-    documents = Document.objects.all()
-
-    context = {
-        'docid': docid,
-        'documents': documents,
-    }
-
-    return render(request, 'open-notes.html', context)
+    return redirect('notes')
