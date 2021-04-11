80a9aeabc36525252aff007dbb3cbc34af3ac681
AbnocChinwads <rnemo@hotmail.co.uk>
2021-04-11 13:13:07 +0000

    database items now editable

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_notes, name='view_notes'),
-    path('add', views.add_notes, name='add_notes'),
+    path('add', views.add_note, name='add_note'),
+    path('edit/<int:document_id>', views.edit_note, name='edit_note'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Document
+from .forms import DocumentForm
 
 
 def view_notes(request):
@@ -17,14 +18,31 @@ def view_notes(request):
     return render(request, 'notes/notes.html', context)
 
 
-def add_notes(request):
+def add_note(request):
     """
     A view to save the newly created document to the database
     """
     if request.method == 'POST':
-        title = request.POST.get('document_title')
-        content = request.POST.get('document_content')
-        Document.objects.create(title=title, content=content)
+        form = DocumentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('view_notes')
+    form = DocumentForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'notes/add_notes.html', context)
 
-        return redirect('view_notes')
-    return render(request, 'notes/add_notes.html')
+
+def edit_note(request, document_id):
+    document = get_object_or_404(Document, id=document_id)
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, instance=document)
+        if form.is_valid():
+            form.save()
+            return redirect('view_notes')
+    form = DocumentForm(instance=document)
+    context = {
+        'form': form
+    }
+    return render(request, 'notes/edit_note.html', context)
