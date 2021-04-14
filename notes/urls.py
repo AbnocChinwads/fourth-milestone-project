c32a3760a10362157b6246080b6014ef59e9750e
AbnocChinwads <rnemo@hotmail.co.uk>
2021-04-14 18:03:46 +0000

    Prettified edit_note.html and add_note.html

--- notes/urls.py
+++ notes/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.view_notes, name='view_notes'),
     path('add', views.add_note, name='add_note'),
+    path('open/<int:document_id>', views.open_note, name='open_note'),
     path('edit/<int:document_id>', views.edit_note, name='edit_note'),
     path('delete/<int:document_id>', views.delete_note, name='delete_note'),
 ]
