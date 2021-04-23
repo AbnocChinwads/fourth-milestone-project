c230c4dffa11e28d175198a885197401205f3af9
AbnocChinwads <rnemo@hotmail.co.uk>
2021-04-23 19:54:58 +0000

    Moved createPaymentMethod function to allow calls to stripe to finish correctly

--- checkout/static/js/stripe_elements.js
+++ checkout/static/js/stripe_elements.js
@@ -57,56 +57,56 @@ function stripeElements() {
         });
     }
 
-}
+    function createPaymentMethod({ card }) {
 
-function createPaymentMethod({ card }) {
+        // Set up payment method for recurring usage
+        let billingName = '{{user.username}}';
 
-    // Set up payment method for recurring usage
-    let billingName = '{{user.username}}';
-
-    stripe
-        .createPaymentMethod({
-            type: 'card',
-            card: card,
-            billing_details: {
-                name: billingName,
-            },
-        })
-        .then((result) => {
-            if (result.error) {
-                displayError(result);
-            } else {
-                const paymentParams = {
-                    price_id: document.getElementById("priceId").innerHTML,
-                    payment_method: result.paymentMethod.id,
-                };
-                fetch("/create-sub", {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-CSRFToken': '{{ csrf_token }}',
-                    },
-                    credentials: 'same-origin',
-                    body: JSON.stringify(paymentParams),
-                }).then((response) => {
-                    return response.json();
-                }).then((result) => {
-                    if (result.error) {
-                        // The card had an error when trying to attach it to a customer
-                        throw result;
-                    }
-                    return result;
-                }).then((result) => {
-                    if (result && result.status === 'active') {
-
-                        window.location.href = '/complete';
+        stripe
+            .createPaymentMethod({
+                type: 'card',
+                card: card,
+                billing_details: {
+                    name: billingName,
+                },
+            })
+            .then((result) => {
+                if (result.error) {
+                    displayError(result);
+                } else {
+                    const paymentParams = {
+                        price_id: document.getElementById("priceId").innerHTML,
+                        payment_method: result.paymentMethod.id,
                     };
-                }).catch(function (error) {
-                    displayError(result.error.message);
+                    fetch("/create-sub", {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'X-CSRFToken': '{{ csrf_token }}',
+                        },
+                        credentials: 'same-origin',
+                        body: JSON.stringify(paymentParams),
+                    }).then((response) => {
+                        return response.json();
+                    }).then((result) => {
+                        if (result.error) {
+                            // The card had an error when trying to attach it to a customer
+                            throw result;
+                        }
+                        return result;
+                    }).then((result) => {
+                        if (result && result.status === 'active') {
 
-                });
-            }
-        });
+                            window.location.href = '/complete';
+                        };
+                    }).catch(function (error) {
+                        displayError(result.error.message);
+
+                    });
+                }
+            });
+    }
+
 }
 
 var changeLoadingState = function (isLoading) {
