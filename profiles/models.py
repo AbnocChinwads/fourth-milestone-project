c9b7bd41d2ba277b1cb606eb2d635f53755f3eff
AbnocChinwads <rnemo@hotmail.co.uk>
2021-04-24 10:17:29 +0000

    Removed test keys

--- profiles/models.py
+++ profiles/models.py
@@ -3,30 +3,17 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from django_countries.fields import CountryField
-
 
 class UserProfile(models.Model):
     """
     A user profile model for maintaining default
     delivery information and order history
     """
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20, null=True,
-                                            blank=True)
-    default_street_address1 = models.CharField(max_length=80, null=True,
-                                               blank=True)
-    default_street_address2 = models.CharField(max_length=80, null=True,
-                                               blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True,
-                                            blank=True)
-    default_county = models.CharField(max_length=80, null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country', null=True,
-                                   blank=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.settings.AUTH_USER_MODEL
+        return self.user
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
