5491e6cd8dc9e8ad531a06bc4bc0a4a2bdbaa4ff
AbnocChinwads <rnemo@hotmail.co.uk>
2021-07-21 22:04:31 +0000

    Adjusted database elif statement in main settings.py

--- fourth_milestone_project/settings.py
+++ fourth_milestone_project/settings.py
@@ -117,18 +117,17 @@ WSGI_APPLICATION = 'fourth_milestone_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if development:
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
